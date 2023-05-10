69adda6c460337c9a60fb23c41fa9094b5c2ae8f
Lucas Lima <lucasmonteiro17@yahoo.com.br>
2023-05-10 12:56:20 -0300

    Solução exercício 2

--- Aprendendo Python/Iniciando na programação Python - Básico/aula32.py
+++ Aprendendo Python/Iniciando na programação Python - Básico/aula32.py
@@ -20,6 +20,17 @@ Faça um programa que pergunte a hora ao usuário e, baseando-se no horário
 descrito, exiba a saudação apropriada. Ex. 
 Bom dia 0-11, Boa tarde 12-17 e Boa noite 18-23.
 """
+entrada = input('Digite a hora em numeros inteiros: ')
+try:
+    hora = int(entrada)
+    if (hora >= 0) and (hora <= 11):
+        print('Bom dia')
+    elif (hora >= 12) and (hora <= 17):
+        print('Boa tarde')
+    elif (hora >= 12) and (hora <= 23):
+        print('Boa noite')
+except:
+    print('Digite um numero inteiro')
 
 """
 Faça um programa que peça o primeiro nome do usuário. Se o nome tiver 4 letras ou 
