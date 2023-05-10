e03591a7686a720e907fc0baa22bbb4ebabc86ee
Lucas Lima <lucasmonteiro17@yahoo.com.br>
2023-05-10 13:24:07 -0300

    Solução exercício 3

--- Aprendendo Python/Iniciando na programação Python - Básico/aula32.py
+++ Aprendendo Python/Iniciando na programação Python - Básico/aula32.py
@@ -37,3 +37,16 @@ Faça um programa que peça o primeiro nome do usuário. Se o nome tiver 4 letra
 menos escreva "Seu nome é curto"; se tiver entre 5 e 6 letras, escreva 
 "Seu nome é normal"; maior que 6 escreva "Seu nome é muito grande". 
 """
+nome = input('Qual o seu nome? ')
+tamanho = len(nome)
+
+
+if tamanho > 1:
+    if tamanho <= 4:
+        print('Seu nome é curto')
+    elif tamanho >=5 and tamanho <= 6:
+        print('Seu nome é normal')
+    elif tamanho > 6:
+        print('Seu nome é muito grande')
+else:
+    print('Digite seu nome corretamente')
